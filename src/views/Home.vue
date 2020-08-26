b21e8ed759d2c53a70094070dce99f2e7a2d4f67
edimo sousa <edimossilva@gmail.com>
2020-08-26 21:23:45 +0300

    6 - add create item modal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <btn label="add"></btn>
+    <create-item-modal></create-item-modal>
   </div>
 </template>
 
 <script>
+import Button from "@/components/Button.vue";
+import CreateItemModal from "@/modals/CreateItemModal.vue";
 export default {
   name: "Home",
-  components: {},
+  components: { btn: Button, "create-item-modal": CreateItemModal },
+  mounted() {
+    console.log("here");
+    this.$modal.show("my-first-modal");
+  },
 };
 </script>
