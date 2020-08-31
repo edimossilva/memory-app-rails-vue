dfe9439e0719160d630114fe1273c2e87c20e665
edimo sousa <edimossilva@gmail.com>
2020-08-31 21:26:41 +0300

    8 - list items

--- tests/e2e/specs/home.js
+++ tests/e2e/specs/home.js
@@ -3,8 +3,16 @@
 describe("Create task", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Home");
+
     cy.get("button").click();
+    cy.get(".create-item-modal-js").should("exist");
+
     cy.contains("label", "Key");
+    cy.get(".create-item-modal--input-key-js").type("key1");
+    cy.get(".create-item-modal--input-value-js").type("value1");
+    cy.get(".create-item-modal--button-create-js").click();
+    cy.get(".create-item-modal-js").should("not.exist");
+
+    cy.contains("key: key1, value: value1");
   });
 });
