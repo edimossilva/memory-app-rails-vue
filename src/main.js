8094aede6da68fda1341ffaf98e0efa0bc6c6fa8
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-09-13 20:46:41 +0300

    15 copy value on click (#16)
    * 15 - adding copy button to item
    
    * fix typo

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VModal from "vue-js-modal";
+import VueClipboard from "vue-clipboard2";
 
 Vue.config.productionTip = false;
 Vue.use(VModal);
+Vue.use(VueClipboard);
 
 new Vue({
   router,
